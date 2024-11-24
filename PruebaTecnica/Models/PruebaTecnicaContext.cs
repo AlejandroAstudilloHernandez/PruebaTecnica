c373fb42ae0b4da96017827d7e593b6b7363c14b
Asther <alejandro.astudillo.hernandez@gmail.com>
2024-11-24 00:32:00 -0700

    Se modifico la cadena de conexión

--- PruebaTecnica/Models/PruebaTecnicaContext.cs
+++ PruebaTecnica/Models/PruebaTecnicaContext.cs
@@ -27,7 +27,7 @@ public partial class PruebaTecnicaContext : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Data Source = PruebaTecnica.mssql.somee.com; Initial Catalog = PruebaTecnica; user id = Hexadecimal_SQLLogin_1; pwd=64i2isfxvu;TrustServerCertificate=True");
+        => optionsBuilder.UseSqlServer("Data Source = PruebaTecnicas.mssql.somee.com; Initial Catalog = PruebaTecnicas; user id = Hexadecimal15_SQLLogin_2; pwd=pfak85b1bf;TrustServerCertificate=True");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
